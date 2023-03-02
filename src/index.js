fd715bdfb0219a17fc3196ef0159f7e0b82b0245
DanielBarragan96 <ie702554@iteso.mx>
2023-03-02 10:41:55 -0600

    showing the column title

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
+import initialData from "./initial-data";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+class App extends React.Component {
+  state = initialData;
+
+  render() {
+    return this.state.columnOrder.map((columnId) => {
+      const column = this.state.columns[columnId];
+      const tasks = column.taskIds.map((taskId) => this.state.tasks[taskId]);
+
+      return column.title;
+    });
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
