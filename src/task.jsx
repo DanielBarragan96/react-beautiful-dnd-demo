f730f33eca0d9e28d792839919aabc8e0a7c273f
DanielBarragan96 <ie702554@iteso.mx>
2023-03-23 13:13:11 -0600

    Reorder Columns with react-beautiful-dnd

--- src/task.jsx
+++ src/task.jsx
@@ -3,22 +3,11 @@ import styled from "styled-components";
 import { Draggable } from "react-beautiful-dnd";
 
 const Container = styled.div`
-  border: 2px solid lightgrey;
-  border-radius: 50%;
+  border: 1px solid lightgrey;
+  border-radius: 2px;
   padding: 8px;
-  margin-right: 8px;
+  margin-bottom: 8px;
   background-color: ${(props) => (props.isDragging ? "lightgreen" : "white")};
-  width: 40px;
-  height: 40px;
-
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  &:focus {
-    outline: none;
-    border-color: red;
-  }
 `;
 
 export default class Task extends React.Component {
@@ -32,7 +21,7 @@ export default class Task extends React.Component {
             innerRef={provided.innerRef}
             isDragging={snapshot.isDragging}
           >
-            {this.props.task.content[0]}
+            {this.props.task.content}
           </Container>
         )}
       </Draggable>
